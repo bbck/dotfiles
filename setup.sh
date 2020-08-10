fa1949cfc2f44a6595d24f3a02ef6618733e2b19
Chris Boyle <chris@bbck.net>
2020-08-09 21:59:28 -0400

    Delete unused Atom config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -eux
 
-xcode-select --install
+# Install Xcode command line developer tools
+if ! xcode-select --print-path &> /dev/null; then
+  xcode-select --install
+fi
 
 # Symlink dotfiles
 for file in *; do
@@ -12,39 +15,15 @@ for file in *; do
   if [[ "${file}" == "README.md" ]]; then
     continue
   fi
-  rm -rf ${HOME}/.${file}
-  ln -s ${HOME}/Developer/dotfiles/${file} ${HOME}/.${file}
+  rm -rf "${HOME}/.${file}"
+  ln -s "${PWD}/${file}" "${HOME}/.${file}"
 done
 
 # Install homebrew
 if [[ ! -x /usr/local/bin/brew ]]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install homebrew packages
 brew install mas
 brew bundle --global
-
-# Install rubies
-rubies=(ruby-1.9.3-p551 ruby-2.3.4 ruby-2.4.1 jruby-1.7.12 jruby-9.1.10.0)
-for ruby in "${rubies[@]}"; do
-  if [[ ! -d ${HOME}/.rubies/${ruby} ]]; then
-    ruby-install ${ruby}
-  fi
-done
-
-# Atom packages
-packages=(
-  ansible-vault
-  busy-signal
-  file-icons
-  intentions
-  language-ansible
-  language-terraform
-  linter
-  linter-ansible-linting
-  linter-rubocop
-  linter-terraform-syntax
-  linter-ui-default
-)
-apm install "${packages[@]}"
