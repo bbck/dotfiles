cff300bdcee3c8a4deafff20815e56c834d2da3d
Chris Boyle <chris@bbck.net>
2021-05-09 23:02:13 -0400

    Slightly better window titles

--- zsh/term.zsh
+++ zsh/term.zsh
@@ -1,11 +1,9 @@
-tab_title() {
+function set_win_title() {
   if $(git rev-parse --is-inside-work-tree 2>/dev/null); then
-    echo -ne "\e]1;$(basename $(git rev-parse --show-toplevel))\a";
+    echo -ne "\033]0; $(basename $(git rev-parse --show-toplevel)) \007"
   else
-    echo -ne "\e]1;${PWD##*/}\a";
+    echo -ne "\033]0; $(basename "$PWD") \007"
   fi
 }
 
-precmd() {
-  tab_title
-}
+precmd_functions+=(set_win_title)
