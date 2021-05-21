c115da64cd0ddd67bf90f301d0cefa3cf33e48be
Chris Boyle <chris@bbck.net>
2021-05-21 18:39:09 -0400

    Better display home dir in tab titles

--- zsh/term.zsh
+++ zsh/term.zsh
@@ -1,5 +1,7 @@
 function set_win_title() {
-  if $(git rev-parse --is-inside-work-tree 2>/dev/null); then
+  if [ "$PWD" = "$HOME" ]; then
+    echo -ne "\033]0; ~ \007"
+  elif $(git rev-parse --is-inside-work-tree 2>/dev/null); then
     echo -ne "\033]0; $(basename $(git rev-parse --show-toplevel)) \007"
   else
     echo -ne "\033]0; $(basename "$PWD") \007"
