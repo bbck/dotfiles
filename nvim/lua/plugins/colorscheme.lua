27fbac074b73cae8ab2e56e9dd81066c4ea293c7
Chris Boyle <chris@bbck.net>
2025-03-03 20:49:13 -0800

    Trying out tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,8 @@
 return {
-	-- install kanagawa
 	{
-		"rebelot/kanagawa.nvim",
+		"folke/tokyonight.nvim",
 		opts = {
-			overrides = function(colors)
-				local ret = {}
-				for i = 1, 6 do
-					-- markdown.nvim
-					ret["RenderMarkdownH" .. i .. "Bg"] = { bg = colors.theme.diff.change }
-				end
-				return ret
-			end,
-		},
-	},
-
-	-- disable included colorschemes
-	{ "catppuccin/nvim", enabled = false },
-	{ "folke/tokyonight.nvim", enabled = false },
-
-	-- set the colorscheme in lazy
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "kanagawa",
+			style = "night",
 		},
 	},
 }
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+	{ "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+	{ "catppuccin/nvim", enabled = false },
+}
