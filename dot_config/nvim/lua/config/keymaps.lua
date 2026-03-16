d521316dc98ff6e7c89fc8a9f912037ad39febb1
Chris Boyle <chris@bbck.net>
2026-03-15 21:58:30 -0700

    Save file keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@ local map = vim.keymap.set
 
 map("n", "<leader>q", "<cmd>qa<cr>", { desc = "Quit" })
 
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
 map("n", "<leader>tf", function() require("util.format").format_on_save() end, { desc = "Toggle Format on Save" })
 
 map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below" })
